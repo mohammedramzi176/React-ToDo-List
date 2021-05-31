05f9f7c044f6fc7cde719207430f079cda392773
ramzi <ramzikvrafikv@gmail.com>
2021-05-31 10:28:37 +0530

    initial commit by Ramzi: React modules are added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          modules added
         </a>
       </header>
     </div>
